a4edd14196fa4112c78fe6c2829773d12a4cd974
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-19 22:47:31 +0300

    Implemented basic routing

---
+++ routes/places-routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+  console.log('GET Request in Places');
+  res.json({message: 'It works!'})
+})
+
+exports.router = router;
