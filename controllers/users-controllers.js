e973af3e0253d10606b7fd0f676295d108fa45f3
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-21 21:32:03 +0300

    Validated patch requests and user routes

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { uuid } = require("uuidv4");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -11,11 +12,15 @@ const DUMMY_USERS = [
   },
 ];
 
-exports. getUsers = (req, res, next) => {
+exports.getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
 exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
   const { name, email, password } = req.body;
 
   const hasUser = DUMMY_USERS.find((u) => u.email === email);
@@ -48,4 +53,3 @@ exports.login = (req, res, next) => {
 
   res.json({ message: "Logged in!" });
 };
-
