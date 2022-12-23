ee339a5ed369029ca285db5ffe9c5ab0b03eee99
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-24 00:31:00 +0300

    Added places retrieval alternative

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -37,9 +37,9 @@ exports.getPlaceById = async (req, res, next) => {
 exports.getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     const error = new HttpError(
       "Something went wrong, could not find places by the provided user id.",
@@ -48,14 +48,14 @@ exports.getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
     );
   }
 
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.map((place) => place.toObject({ getters: true })),
   });
 };
 
