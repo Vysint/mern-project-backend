ac7b58a6a6fce0ab65a33d4c9591b509a1fa6184
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-23 22:47:35 +0300

    Added/created user using the User model

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,53 +1,78 @@
 // const uuid = require('uuid/v4');
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
-const HttpError = require('../models/http-error');
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
   {
-    id: 'u1',
-    name: 'Max Schwarz',
-    email: 'test@test.com',
-    password: 'testers'
-  }
+    id: "u1",
+    name: "Max Schwarz",
+    email: "test@test.com",
+    password: "testers",
+  },
 ];
 
 exports.getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
-  const hasUser = DUMMY_USERS.find(u => u.email === email);
-  if (hasUser) {
-    throw new HttpError('Could not create user, email already exists.', 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+  if (existingUser) {
+    const error = new HttpError(
+      "User exists already, please login instead.",
+      422
+    );
+    return next(error);
   }
 
-  const createdUser = {
-    // id: uuid(),
-    name, // name: name
+  const createdUser = new User({
+    name,
     email,
-    password
-  };
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Chris_Brown_5%2C_2012.jpg/440px-Chris_Brown_5%2C_2012.jpg",
+    password,
+    places,
+  });
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
 
-  res.status(201).json({user: createdUser});
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
   if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+    throw new HttpError(
+      "Could not identify user, credentials seem to be wrong.",
+      401
+    );
   }
 
-  res.json({message: 'Logged in!'});
+  res.json({ message: "Logged in!" });
 };
-
